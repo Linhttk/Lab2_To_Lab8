925e8e2d84ce4762c69a99d0cda53650384e82f9
Linhttk <55974911+Linhttk@users.noreply.github.com>
2020-10-12 09:38:05 +0700

    Add files via upload

---
+++ Test Suites/TestSuite_LoadData.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Lab08</description>
+   <name>TestSuite_LoadData</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8b85f126-dabe-4585-83ca-7e7cb17789a7</testSuiteGuid>
+   <testCaseLink>
+      <guid>d033be7f-8ab8-4efd-9d27-39d8728e838b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_LoadDataExcel</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>d39dab86-7991-42a8-8981-1d92537d4dce</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Test Data File Excel</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>d39dab86-7991-42a8-8981-1d92537d4dce</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>url</value>
+         <variableId>8a0a6caa-0a48-4261-9217-ccdd38d4e295</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>d39dab86-7991-42a8-8981-1d92537d4dce</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>3733cb08-489e-4dcb-9390-7e59f47ea4f7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>d39dab86-7991-42a8-8981-1d92537d4dce</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>ea140d58-9022-4a26-a138-0d5844949e0d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TestSuites_Login.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>add 5 test case login</description>
+   <name>TestSuites_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>95cc570a-cd56-4e03-83c7-4a1ab264daae</testSuiteGuid>
+   <testCaseLink>
+      <guid>44f7d7e9-8b69-40a3-8bfd-5bef8df386f5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a41be58a-155c-44a7-9d0e-9c8187a80ddc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>da9b855f-a5f6-429a-a2c8-37cd6f222215</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase3</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d6301d8d-afe9-4430-8f50-6d691fc05cc8</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>77126842-1631-4876-9924-6af6f84a6d95</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>109a65cd-4f5a-428d-b535-d3bd51898059</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>409648a8-7ef0-41a1-a362-955121b3683c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e78b08f0-856f-4459-aa2b-0375d8753f20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
